2793e9a1a1c2a8009f0811ead90f4dc514383519
Riad Quliyev <youlo13371488@gmail.com>
2023-06-23 17:13:42 +0400

    Init commit

---
+++ resources/views/front/layouts/app.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<!--
+	Massively by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Generic Page - Massively by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+                <header id="header">
+                    <a href="index.html" class="logo">Massively</a>
+                </header>
+
+				<!-- Nav -->
+					<x-nav/>
+                    
+				<!-- Main -->
+					@yield('main')
+				<!-- Footer -->
+					
+                    <x-footer/>
+				<!-- Copyright -->
+					<div id="copyright">
+						<ul><li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<x-scripts/>
+
+	</body>
+</html>
